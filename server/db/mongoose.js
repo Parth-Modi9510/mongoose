64334a8e27404a507e418e1b16bfdc628c107d88
Parth-Modi9510 <parth.modi9510@gmail.com>
2018-01-31 15:29:08 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
